9993650c66117b5d12a62c2f5bedc2a5f8566cae
Madhav Baliga <42736071+mbaliga@users.noreply.github.com>
2026-08-02 17:40:39 +0530

    app: add Compose host activity

---
+++ app/src/main/java/io/github/mbaliga/fylz/MainActivity.kt
@@ -0,0 +1,15 @@
+package io.github.mbaliga.fylz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import io.github.mbaliga.fylz.ui.FylzApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent { FylzApp() }
+    }
+}
